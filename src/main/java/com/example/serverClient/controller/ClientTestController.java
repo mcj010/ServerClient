e17c37fdf4c0c5e404aeade5c7f3a059082edcb9
329392494@qq.com <329392494@qq.com>
2020-08-11 17:36:43 +0800

    chore(Others): create project.

---
+++ src/main/java/com/example/serverClient/controller/ClientTestController.java
@@ -0,0 +1,26 @@
+package com.example.serverClient.controller;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Configuration
+public class ClientTestController {
+
+    @Bean
+   @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/getTestMsg")
+    public String getTestMsg(){
+        RestTemplate rt = getRestTemplate();
+        String result = rt.getForObject("http://ServerProvide/ServerTest", String.class);
+        return result;
+    }
+}
---
+++ src/test/java/com/example/serverClient/ServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
